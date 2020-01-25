97f559005feb19cb24e1a3d7bab12dfdf836848b
rikuya.okamoto <pgbb.oklock@gmail.com>
2020-01-25 17:57:38 +0900

    Add pagination

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -36,10 +36,24 @@ public class IndexServlet extends HttpServlet {
         // TODO Auto-generated method stub
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Tasklist> tasklist = em.createNamedQuery("getAllTasklists", Tasklist.class).getResultList();
+        int page = 1;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        }catch(NumberFormatException e){}
+
+        List<Tasklist> tasklist = em.createNamedQuery("getAllTasklists", Tasklist.class)
+                                     .setFirstResult(15 * (page -1))
+                                     .setMaxResults(15)
+                                     .getResultList();
+
+        long tasklist_count = (long)em.createNamedQuery("getTasklistsCount", Long.class)
+                                      .getSingleResult();
+
         em.close();
 
         request.setAttribute("tasklist", tasklist);
+        request.setAttribute("tasklist_count", tasklist_count);
+        request.setAttribute("page", page);
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklist/index.jsp");
         rd.forward(request, response);
